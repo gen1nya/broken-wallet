8791b747f62bb01d3c5839595c6175ffeae96f48
Evgeniy Andriyanoff <gen1nya@gmail.com>
2025-11-25 18:08:27 +0400

    Load Buffer polyfill before app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
+import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Buffer } from 'buffer';
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import App from './App';
-
-// Ensure Buffer is available in the browser for bitcoinjs-lib usage
-if (!globalThis.Buffer) {
-  globalThis.Buffer = Buffer;
-}
 
 const theme = extendTheme({
   config: {
